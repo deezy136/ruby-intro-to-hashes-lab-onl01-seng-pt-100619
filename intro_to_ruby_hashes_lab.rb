acd139dea274170084089b632541a706f6ecf82b
deezy13 <deezy13@gmail.com>
2019-10-22 03:40:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,11 +51,11 @@ monopoly = {
       pieces: 4,
       rent_in_dollars: {
         one_piece_owned:      25, 
-# alt  :one_piece_owned =>    25,  <===this will give you the same result         
+        
         two_pieces_owned:     50,
-# alt  :two_pieces_owned =>   25,  <===this will give you the same result 
+
         three_pieces_owned:   100, 
-# alt  :three_pieces_owned => 100, <===this will give you the same result 
+
         four_pieces_owned:    200
 # alt  :four_pieces_owned =>  200  <===this will give you the same result 
       }, 
