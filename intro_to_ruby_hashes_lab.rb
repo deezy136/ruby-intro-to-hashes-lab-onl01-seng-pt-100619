4f2d76858f0fe0ab53ae738276d86f5626328a21
deezy13 <deezy13@gmail.com>
2019-10-22 03:41:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,14 +51,12 @@ monopoly = {
       pieces: 4,
       rent_in_dollars: {
         one_piece_owned:      25, 
-        
         two_pieces_owned:     50,
-
         three_pieces_owned:   100, 
-
         four_pieces_owned:    200
-# alt  :four_pieces_owned =>  200  <===this will give you the same result 
+
       }, 
+      
       names: {
         reading_railroad: {},
         pennsylvania_railroad: {},
